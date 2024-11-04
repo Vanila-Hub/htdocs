6728abe33b6cc01303b26c87777fe2138c7b52ba
Juan Antero Asumu <149675635+Vanila-Hub@users.noreply.github.com>
2024-11-04 11:36:15 +0100

    gg

--- Dwes/ProyectoVideoclub/public/login.php
+++ Dwes/ProyectoVideoclub/public/login.php
@@ -1,17 +1,29 @@
 <?php
 require_once __DIR__ . '/../autoload.php';
 use Dwes\ProyectoVideoclub\app\Cliente;
+use Dwes\ProyectoVideoclub\app\Soporte;
 
 session_start();
 
 // Inicializa los clientes solo si no existen en la sesión
 if (!isset($_SESSION['clientes'])) {
+    // Inicializa los soportes
+$soportes = [
+    new Soporte('Star Wars', 1, 10),
+    new Soporte('Matrix', 2, 15),
+    new Soporte('FIFA 2022', 3, 60),
+];
+
     $clientes = [
         new Cliente('Juan Perez', 1, 'juan', '1234', 3),
         new Cliente('Maria Lopez', 2, 'maria', '5678', 2),
         new Cliente('usuario', 3, "usuario", "usuario", 2),
         new Cliente('user', 4, "user", "user", 2),
     ];
+
+      $clientes[0]->alquilar($soportes[0]);  
+      $clientes[0]->alquilar($soportes[1]);  
+      $clientes[1]->alquilar($soportes[2]);  
 
     // Convierte los objetos Cliente a JSON y los almacena en la sesión
     $_SESSION['clientes'] = array_map(function($cliente) {
